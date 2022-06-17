6b30b27617095487f13ec9bd20a975bc3b09d9bd
SarahOh92 <201124081oek@gmail.com>
2022-06-17 12:51:54 +0900

    gnb 수정 및 폰트 수정

--- js/stlye.js
+++ js/stlye.js
@@ -8,3 +8,14 @@ $(function () {
       $(this).removeClass("active");
     });
   });
+  $(function(){
+    $(".gnb").on("mouseenter", function(){
+      $(".sub_bg, .depth2").addClass("on");
+    });
+      
+    $(".gnb").on("mouseleave", function(){
+      setTimeout(function(){
+         $(".sub_bg, .depth2").removeClass("on");
+     }, 100);
+  });
+  });
