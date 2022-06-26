569b9125b3598be545068b997ce5cecbdd6165d2
SarahOh92 <201124081oek@gmail.com>
2022-06-26 17:28:31 +0900

    gnb 수정 및 사이즈 수정

--- js/stlye.js
+++ js/stlye.js
@@ -11,11 +11,13 @@ $(function () {
   $(function(){
     $(".gnb_area").on("mouseenter", function(){
       $(".sub_bg, .depth2").addClass("on");
+      $(".gnb_area").addClass("hover");
     });
       
     $(".gnb_area").on("mouseleave", function(){
       setTimeout(function(){
          $(".sub_bg, .depth2").removeClass("on");
+         $(".gnb_area").removeClass("hover");
      }, 50);
   });
   });
